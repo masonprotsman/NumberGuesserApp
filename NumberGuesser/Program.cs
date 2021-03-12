a12bb85f5a8d8430e072ce08a31547f3e8e51db5
masonprotsman <33502238+masonprotsman@users.noreply.github.com>
2021-03-12 15:11:33 -0500

    Add files via upload

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
+// namespace
 namespace NumberGuesser
 {
+    // Main class
     class Program
     {
+        // Entry Point Method
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Set App Vars
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string authorName = "Mason Protsman";
+
+            // Change text color
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            // Write out app info
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, authorName);
+
+            // Reset Color
+            Console.ResetColor();
         }
     }
 }
