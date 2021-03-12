5752959bde8b1fa59fe7b1d905dd50737ad639bc
masonprotsman <33502238+masonprotsman@users.noreply.github.com>
2021-03-12 15:29:05 -0500

    Add files via upload

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -15,7 +15,7 @@ namespace NumberGuesser
             string authorName = "Mason Protsman";
 
             // Change text color
-            Console.ForegroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Blue;
 
             // Write out app info
             Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, authorName);
@@ -30,6 +30,44 @@ namespace NumberGuesser
             string userName = Console.ReadLine();
 
             Console.WriteLine("Hello {0}, let's play a game...", userName);
+
+            // Set Correct Number
+            int correctNumber = 7;
+
+            // Init Guess Var
+            int userGuess = 0;
+
+            // Prompt User For Number
+            Console.WriteLine("Guess a number between 1 and 10");
+
+            // While
+            while(userGuess != correctNumber)
+            {
+                //Get User Input
+                string input = Console.ReadLine();
+
+                // Cast Input To Int
+                userGuess = Int32.Parse(input);
+
+                // Check User Guess
+                if(userGuess != correctNumber)
+                {
+                    // Change text color
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    // Write out Wrong message
+                    Console.WriteLine("Wrong answer, please try again");
+
+                    // Reset Color
+                    Console.ResetColor();
+                }
+
+            }
+            // Change text color
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            // Write out correct message
+            Console.WriteLine("Your guess is correct. You win!!!");
         }
     }
 }
